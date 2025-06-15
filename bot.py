6549cbaeaf096d992cba09ae7ee6b134e3d622d5
Denis <larrrrrryanderson@gmail.com>
2025-06-15 21:46:09 +0300

    nothing

--- bot.py
+++ bot.py
@@ -16,6 +16,7 @@ async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)  
 
+
 if __name__ == "__main__":
     #Логирование лучше убрать так как треубет много ресурсов в продакшене
     logging.basicConfig(level=logging.INFO) 
